67da0799fb2a10377372e77e0044d032460242d9
Mario Orlandi <morlandi@brainstorm.it>
2013-05-30 18:10:47 +0200

    extraadminfilters added

--- project/project/admin.py
+++ project/project/admin.py
@@ -2,13 +2,21 @@ from django.contrib import admin
 from project.models import Continent
 from project.models import Language
 from project.models import Country
+from extraadminfilters.filters import UnionFieldListFilter
 
 
 class CountryAdmin(admin.ModelAdmin):
     list_display = ['description', 'continent', 'list_languages', ]
     filter_horizontal = ['languages', ]
-    list_filter = ['continent', 'languages', ]
-    multiple_selection_list_filter = ['continent', 'languages', ]
+
+    # list_filter = ['continent', 'languages', ]
+    # multiple_selection_list_filter = ['continent', 'languages', ]
+
+    list_filter = (
+        #('continent', UnionFieldListFilter),
+        'continent',
+        ('languages', UnionFieldListFilter),
+    )
 
     def list_languages(self, instance):
         return ', '.join([language.description for language in instance.languages.all()])
